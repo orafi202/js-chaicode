29fc2c28746beadf6fe7fb1fe256a7ab022b84f0
Muhammad Rafi <orafi202@gmail.com>
2025-07-08 23:37:12 +0000

    DateTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 14455
+let accountEmail = "orafi202@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+accountEmail = 'abc@gmail.com'
+accountPassword = "12345"
+let accountState
+
+
+
+// accountId = 16331 reassignment of const variable cannot be done.
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+
+we cannot use var for variable because var does not support block level and function level scope.
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+let name = 'Muhammad Raf' // string
+let age = '23' // number
+let AccountBalance = 234.34 // floating number
+let isLoggedIn = true // boolean 
+let state = null // 
+let savePassword ; // undefined
+
