ff060ae9423d38e928e4de0cfc22ddf1cea963a2
Muhammad Rafi <orafi202@gmail.com>
2025-07-09 23:45:14 +0000

    conversion operations to avoid confusion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,3 +5,16 @@ let ageInString = String(age);
 
 console.log(ageInString, typeof(ageInString));
 
+
+console.log(2 + 2 + "1")
+console.log( "1" + 2 + 2 )
+
+
+console.log(-true) // assigning negative value and converting boolean value in number
+console.log(+"") // converting empty string to number 0
+
+num = 10
+console.log(num) // before incremeting
+let newNum = num++ //  11 
+console.log(num++, newNum)
+console.log(num) // 12
