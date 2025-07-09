e12b359661f798da1311d49c41df7d77a4667d10
Muhammad Rafi <orafi202@gmail.com>
2025-07-09 22:40:05 +0000

    adding new source code

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,4 +4,18 @@ let AccountBalance = 234.34 // floating number
 let isLoggedIn = true // boolean 
 let state = null // 
 let savePassword ; // undefined
+let a  = 6
+let b = 3
 
+
+/*
+   Arthimetic Operations
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+ */
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,7 @@
+let name = 'Muhammad Rafi'
+let age = 34
+
+let ageInString = String(age);
+
+console.log(ageInString, typeof(ageInString));
+
